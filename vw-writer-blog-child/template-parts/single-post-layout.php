7b27813c57650480feb95feb85848e3bd13c07fd
= <=>
2019-07-03 09:58:13 +0700

    delete avatar in post

--- vw-writer-blog-child/template-parts/single-post-layout.php
+++ vw-writer-blog-child/template-parts/single-post-layout.php
@@ -12,16 +12,14 @@
         <?php if(get_theme_mod('vw_writer_blog_toggle_postdate',true)==1){ ?>
             <span class="entry-date"><?php the_date(); ?></span><span>|</span>
         <?php } ?>
+        
         <?php if(get_theme_mod('vw_writer_blog_toggle_comments',true)==1){ ?>  
             <span class="entry-comments"><?php comments_number( __('0 Comment', 'vw-writer-blog'), __('0 Comments', 'vw-writer-blog'), __('% Comments', 'vw-writer-blog') ); ?> </span>
         <?php } ?>
     </div>
     <h2><?php the_title(); ?></h2>
-    <?php if(has_post_thumbnail()) { ?>
-        <div class="feature-box">   
-            <img src="<?php the_post_thumbnail_url('full'); ?>">
-        </div>
-        <?php } the_content();
+
+        <?php  the_content();
         the_tags(); ?>
         <?php
         // If comments are open or we have at least one comment, load up the comment template
